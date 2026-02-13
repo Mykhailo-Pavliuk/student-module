1081b23c99acc6021f70731b8581d3df2948bc94
Anastasiia Pavliuk <pavliuk.anstasia05@gmail.com>
2026-02-13 21:02:33 +0200

    [SP-22] Create Student class inside Source.cpp

--- app/StudentModule/Source.cpp
+++ app/StudentModule/Source.cpp
@@ -7,15 +7,55 @@ private:
 	string surname;
 	int age;
 	int course;
-public:
-	Student(string name, string surname, int age, int course) {
-		this->name = name;
-		this->surname = surname;
-		this->age = age;
-		this->course = course;
+
+	void setName(string name) {
+		if (name.length() < 2) {
+			cout << "Invalid name value" << endl;
+			this->name = "Unknown";
+		}
+		else {
+			this->name = name;
+		}
 	}
 
-	string getName(){
+	void setSurname(string surname) {
+		if (surname.length() < 2) {
+			cout << "Invalid surname value" << endl;
+			this->surname = "Unknown";
+		}
+		else {
+			this->surname = surname;
+		}
+	}
+
+	void setAge(int age) {
+		if (age >=18 && age <= 120) {
+			this->age = age;
+		}
+		else {
+			cout << "Invalid age value" << endl;
+			this->age = 18;
+		}
+	}
+
+	void setCourse(int course) {
+		if (course > 0 && course <= 6) {
+			this->course = course;
+		}
+		else {
+			cout << "Invalid course value" << endl;
+			this->course = 1;
+		}
+	}
+public:
+	Student(string name, string surname, int age, int course) {
+		setName(name);
+		setSurname(surname);
+		setAge(age);
+		setCourse(course);
+	}
+
+	string getName() {
 		return name;
 	}
 
@@ -34,7 +74,12 @@ public:
 	void print() {
 		cout << "Name: " << name << endl;
 		cout << "Surname: " << surname << endl;
-		cout << "Age: " << age << " years old" << endl;
+		if (age == 1) {
+			cout << "Age: " << age << " year old" << endl;
+		}
+		else {
+			cout << "Age: " << age << " years old" << endl;
+		}
 		cout << "Course: " << course << endl;
 	}
 };
@@ -42,6 +87,6 @@ public:
 int main() {
 	Student student("Nastya", "Pavliuk", 19, 1);
 	student.print();
-	
+
 	return 0;
 }
