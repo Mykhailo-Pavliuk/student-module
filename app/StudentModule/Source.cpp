24db934589d602cde604948af626a947356bdaf6
pavliuk-anastasia <pavliuk.anastasia05@gmail.com>
2026-02-16 16:41:11 +0200

    [SP-22] Create Student class inside Source.cpp (#5)
    * [SP-22] Create Student class inside Source.cpp
    
    * [SP-22] Create Student class inside Source.cpp
    
    ---------
    
    Co-authored-by: Anastasiia Pavliuk <pavliuk.anstasia05@gmail.com>

--- app/StudentModule/Source.cpp
+++ app/StudentModule/Source.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+class Student {
+private:
+	string name;
+	string surname;
+	int age;
+	int course;
+public:
+	Student(string name, string surname, int age, int course) {
+		this->name = name;
+		this->surname = surname;
+		this->age = age;
+		this->course = course;
+	}
 
+	string getName(){
+		return name;
+	}
+
+	string getSurname() {
+		return surname;
+	}
+
+	int getAge() {
+		return age;
+	}
+
+	int getCourse() {
+		return course;
+	}
+
+	void print() {
+		cout << "Name: " << name << endl;
+		cout << "Surname: " << surname << endl;
+		cout << "Age: " << age << " years old" << endl;
+		cout << "Course: " << course << endl;
+	}
+};
+
+int main() {
+	Student student("Nastya", "Pavliuk", 19, 1);
+	student.print();
 	
 	return 0;
 }
