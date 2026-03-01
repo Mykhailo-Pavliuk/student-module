73d9b452cd1e876b578788a31ef46a2635f84bdc
Anastasiia Pavliuk <pavliuk.anstasia05@gmail.com>
2026-03-01 10:38:37 +0200

    SP-49: СhatGPT task-1

---
+++ tasks/СhatGPT task-1/Source.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+class Cube {
+private:
+	double lengh;
+	double widht;
+	double heigh;
+	double weight;
+	string color;
+public:
+	Cube() {
+		this->lengh = 1;
+		this->widht = 1;
+		this->heigh = 1;
+		this->weight = 1;
+		this->color = "Red";
+	}
+
+	Cube(double l, double w, double h, double wei, string c) {
+		init(l, w, h, wei, c);
+	}
+	
+	void init(double l, double w, double h, double wei, string c) {
+		if (l > 0 && l <= 15 &&
+			w>0 && w <= 15 &&
+			h>0 && h <= 15 &&
+			wei>0 && wei <= 15) {
+
+			this->lengh = l;
+			this->widht = w;
+			this->heigh = h;
+			this->weight = wei;
+			this->color = c;
+		}
+		else {
+			lengh = 1;
+			widht = 1;
+			heigh = 1;
+			weight = 1;
+			color = "Red";
+		}
+
+		if (c == "Yellow" || c == "Blue" || c == "Green" || c == "Pink" || c == "Black")
+			color = c;
+		else
+			color = "Red";
+	}
+
+	void read() {
+		double l, w, h, wei;
+		string c;
+	
+		cout << "Enter lengh: " << endl;
+		cin >> l;
+
+		cout << "Enter widht: " << endl;
+		cin >> w;
+
+		cout << "Enter heigh of cube: " << endl;
+		cin >> h;
+
+		cout << "Enter weight of cube: " << endl;
+		cin >> wei;
+
+		cout << "Enter color of cube: " << endl;
+		cin >> c;
+
+		init(l, w, h, wei, c);
+	}
+
+	double volume() {
+		return lengh * widht * heigh;
+	}
+
+	double shstilnist() {
+		return weight / volume();
+	}
+
+	bool fitsInBox(double boxL, double boxW, double boxH) {
+		return (lengh <= boxL &&
+			widht <= boxW &&
+			heigh <= boxH);
+	}
+
+	void display() {
+		cout << "\nCube data:\n";
+		cout << "Lengh: " << lengh << endl;
+		cout << "Widht: " << widht << endl;
+		cout << "Heigh: " << heigh << endl;
+		cout << "Weight: " << weight << endl;
+		cout << "Color: " << color << endl;
+		cout << endl;
+		cout << "Volume: " << volume() << endl;
+		cout << "Density: " << shstilnist() << endl;
+	}
+};
+
+int main() {
+	Cube cube;
+	cube.read();
+	cube.display();
+
+	if (cube.fitsInBox(10, 10, 10)) {
+		cout << "Cube fits in box 10x10x10 ";
+	}
+	else {
+		cout << "Cube does NOT fit in box 10x10x10 ";
+	}
+
+	return 0;
+}
